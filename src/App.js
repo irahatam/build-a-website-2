d00bed00fc57812b81a601d7d92b86c278a9c728
Matahari Kinanti Herwin <matahari@nyu.edu>
2020-12-29 14:30:19 +0700

    added router to app.js, customized city names in header.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+
+import Home from "./containers/Home";
 
 function App() {
   return (
-    <div className="App bg-blue-200 h-screen flex items-center justify-center">
-      <header className="App-header">
-        <p className="text-2xl">
-          Edit <code>src/Home.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link text-sm font-light"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          This is the weather-app starter code for Build a Website II
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,13 +4,13 @@ function Header() {
   return (
     <header>
       <div>
-        <h1> TODO </h1>
+        <h1> Matahari's Weather App </h1>
       </div>
       <nav>
-        <a href="/?city=TODO"> City Name </a>
-        <a href="/?city=TODO"> City Name </a>
-        <a href="/?city=TODO"> City Name </a>
-        <a href="/?city=TODO"> City Name </a>
+        <a href="/?city=Jakarta"> Jakarta </a>
+        <a href="/?city=New%20York"> New York </a>
+        <a href="/?city=Tokyo"> Tokyo </a>
+        <a href="/?city=Sydney"> Sydney </a>
       </nav>
     </header>
   );
