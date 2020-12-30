4c4f35481c95325b2d571730cf6eae3021f4880b
Arda Satata <ardasatata@gmail.com>
2020-12-30 13:11:47 +0800

    make a new component City.js, modify the Home.js

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -5,15 +5,18 @@ import React, { useState, useEffect, useMemo } from "react";
 import Header from "../components/Header";
 import WeatherImage from "../components/WeatherImage";
 
+import City from "../components/City";
+
 const weatherKey = `TODO`; // Your API Key here
 
 function Home() {
   // TODO
   return (
-    <div className="flex flex-col h-screen bg-green-200 text-4xl">
-      <div className="p-8 bg-white mb-24">HALO</div>
-      <div className="p-8 bg-red-500">HALO</div>
-      <div className="p-8 bg-blue-500">BANDUNG</div>
+    // Container
+    <div className="flex flex-col h-screen bg-green-200">
+      <City cityName={"Tokyo"} temp={"10°C"} color={"bg-yellow-500"} />
+      <City cityName={"Jakarta"} temp={"24°C"} color={"bg-red-500"} />
+      <City cityName={"Bandung"} temp={"17°C"} color={"bg-blue-500"} />
     </div>
   );
 }
