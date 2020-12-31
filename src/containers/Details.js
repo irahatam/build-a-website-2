b7ce61477ec9adbd9b3c6c4d527d4988b38a227e
Arda Satata <ardasatata@gmail.com>
2020-12-31 15:30:47 +0800

    update details.js

--- src/containers/Details.js
+++ src/containers/Details.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect, useMemo } from "react";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
 
 // TODO
 // - implement API
@@ -6,21 +8,89 @@ import React, { useState, useEffect, useMemo } from "react";
 // - style the details screen
 
 function Details() {
+  const history = useHistory();
+  const [weatherData, setWeatherData] = useState(null);
+  const [city, setCity] = useState("Jakarta");
+
+  useEffect(() => {
+    console.log(process.env.REACT_APP_WEATHER_KEY);
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.REACT_APP_WEATHER_KEY}`
+      )
+      .then(function (response) {
+        // Successful request
+        const weather = response.data;
+        setWeatherData(weather);
+      })
+      .catch(function (error) {
+        // The best practice of coding is to not use console.log
+        console.log(error);
+      });
+  }, [city]);
+
+  useEffect(() => {
+    const searchParams = history.location.search;
+    const urlParams = new URLSearchParams(searchParams);
+    const cityName = urlParams.get("name");
+    if (city) {
+      setCity(city);
+    }
+  }, [history]);
+
+  const {
+    cloudiness,
+    currentTemp,
+    highTemp,
+    humidity,
+    lowTemp,
+    weatherType,
+    windSpeed,
+  } = useMemo(() => {
+    let cloudiness = "";
+    let currentTemp = "";
+    let highTemp = "";
+    let humidity = "";
+    let lowTemp = "";
+    let weatherType = "";
+    let windSpeed = "";
+
+    if (weatherData) {
+      cloudiness = `${weatherData.clouds.all}%`;
+      currentTemp = `${Math.round(weatherData.main.temp)}°C`;
+      highTemp = `${Math.round(weatherData.main.temp_max)}°C`;
+      humidity = `${weatherData.main.humidity}%`;
+      lowTemp = `${Math.round(weatherData.main.temp_min)}°C`;
+      weatherType = `${weatherData.weather[0].description}`;
+      windSpeed = `${weatherData.wind.speed} mph`;
+    }
+
+    return {
+      cloudiness,
+      currentTemp,
+      highTemp,
+      humidity,
+      lowTemp,
+      weatherType,
+      windSpeed,
+    };
+  }, [weatherData]);
+
   return (
     // Container
     <div className="flex flex-col items-center h-screen bg-green-200">
-      <div className="p-8 text-2xl font-bold">Weather in CityName</div>
+      <div className="p-8 text-2xl font-bold">Weather in {city}</div>
 
       <div className="flex flex-col p-8 m-4 border-2 rounded-md border-gray-700 items-center">
-        <div>haze</div>
-        <div>Current Temperature : 26</div>
+        <div>{weatherType}</div>
+        <div>Current Temperature : {currentTemp}</div>
       </div>
 
-      <div>High Temperature : 26</div>
-      <div>Cloudiness : 20%</div>
-      <div>Low Temperature : 18</div>
-      <div>Humidity : 83%</div>
-      <div>Wind Speed : 3.1 mph</div>
+      <div>High Temperature : {highTemp}</div>
+      <div>Cloudiness : {cloudiness}</div>
+      <div>Low Temperature : {lowTemp}</div>
+      <div>Humidity : {humidity}</div>
+      <div>Wind Speed : {windSpeed}</div>
     </div>
   );
 }
