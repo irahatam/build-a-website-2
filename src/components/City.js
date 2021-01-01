bf8c05d7fa96502568eedd8a890db5dea76263bc
Arda Satata <ardasatata@gmail.com>
2021-01-01 11:00:30 +0800

    update list to fetch weather data

--- src/components/City.js
+++ src/components/City.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useHistory } from "react-router-dom";
 
 // props -> cityName ,temp, color
-function City({ cityName, temp, color }) {
+function City({ cityName, weatherType, temp = 0, color }) {
   const history = useHistory();
 
   function handleClick() {
@@ -14,8 +14,12 @@ function City({ cityName, temp, color }) {
       className={"flex flex-row p-8 justify-between items-center " + color}
       onClick={handleClick}
     >
-      <div className="text-2xl font-light">{cityName}</div>
-      <div className="text-5xl">{temp}</div>
+      <div className="flex flex-col">
+        <div className="text-md font-light text-left">{weatherType}</div>
+        <div className="text-2xl font-light">{cityName}</div>
+      </div>
+
+      <div className="text-5xl">{Math.round(temp)} °C</div>
     </button>
   );
 }
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -13,18 +13,33 @@ function Home() {
     {
       name: "Jakarta",
       currentTemp: "0",
+      weatherType: "",
       color: "bg-yellow-500",
     },
     {
       name: "Bandung",
       currentTemp: "0",
+      weatherType: "",
       color: "bg-red-500",
     },
     {
       name: "Malang",
       currentTemp: "0",
+      weatherType: "",
       color: "bg-blue-500",
     },
+    // {
+    //   name: "Taoyuan",
+    //   currentTemp: "0",
+    //   weatherType: "",
+    //   color: "bg-blue-500",
+    // },
+    // {
+    //   name: "New York",
+    //   currentTemp: "0",
+    //   weatherType: "",
+    //   color: "bg-blue-500",
+    // },
   ]);
 
   useEffect(() => {
@@ -62,11 +77,58 @@ function Home() {
     };
   }, [weatherData]);
 
+  useEffect(() => {
+    updateAllWeatherData();
+  }, []);
+
+  async function fetchWeatherData(cityName) {
+    const res = await axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${process.env.REACT_APP_WEATHER_KEY}`
+      )
+      .then(function (response) {
+        // Successful request
+        const weather = response.data;
+        return weather;
+      })
+      .catch(function (error) {
+        // The best practice of coding is to not use console.log
+        console.warn(error);
+      });
+
+    return res;
+  }
+
+  async function updateAllWeatherData(params) {
+    cities.forEach(function (citiesItems, index) {
+      let weatherData = {};
+      let newCities = [...cities];
+
+      fetchWeatherData(citiesItems.name).then((res) => {
+        weatherData = res;
+        console.log(weatherData);
+
+        newCities[index].currentTemp = weatherData.main.temp;
+        newCities[index].weatherType = weatherData.weather[0].main;
+        setCities(newCities);
+      });
+    });
+  }
+
   return (
     // Container
-    <div className="flex flex-col h-screen bg-green-200">
+    <div className="flex flex-col h-screen bg-green-100">
+      <div className="text-4xl text-yellow-500 font-semibold text-center my-4">
+        Our Weather App
+      </div>
+
       {cities.map((item, index) => (
-        <City cityName={item.name} temp={item.currentTemp} color={item.color} />
+        <City
+          cityName={item.name}
+          weatherType={item.weatherType}
+          temp={item.currentTemp}
+          color={item.color}
+        />
       ))}
     </div>
   );
